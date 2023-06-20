e9c2acd34f83b443256ef176561906348f25bc7e
SAGAR SATAPATHY <wishsagarks@gmail.com>
2023-06-20 22:08:39 +0530

    removed bugs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
+      <Head />
 
-      </Head>
       <body>
         <Main />
         <NextScript />
-        <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js' />
-        <Script  
-          id="freakingwish" 
+        <Script
+          src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
+          strategy="lazyOnload"
+        />
+        <Script
+          id="freakingwish"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html:
-            `kofiWidgetOverlay.draw('freakingwish', {
-              'type': 'floating-chat',
-              'floating-chat.donateButton.text':'Tip Me',
-              'floating-chat.donateButton.background-color': '#00b9fe',
-              'floating-chat.donateButton.text-color': '#fff'
-            });`
+            __html: `
+              kofiWidgetOverlay.draw('freakingwish', {
+                'type': 'floating-chat',
+                'floating-chat.donateButton.text':'Tip Me',
+                'floating-chat.donateButton.background-color': '#00b9fe',
+                'floating-chat.donateButton.text-color': '#fff'
+              });
+            `,
           }}
         />
       </body>
-    </Html >
-  )
+    </Html>
+  );
 }
